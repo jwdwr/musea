7d8989480503b8702202c6628a80dfc0cbbe1562
aswever <aswever@protonmail.com>
2023-07-25 11:53:48 -0400

    fix mirrors

--- components/Room/RoomWall.tsx
+++ components/Room/RoomWall.tsx
@@ -1,5 +1,5 @@
-import React, { useMemo, useRef } from "react";
-import { BoxGeometry, Group, MeshStandardMaterial, PlaneGeometry, Vector3 } from "three";
+import React, { useMemo } from "react";
+import { BoxGeometry, MeshStandardMaterial, PlaneGeometry, Vector3 } from "three";
 import { RigidBody } from "@react-three/rapier";
 import { Direction } from "@/lib/shared/museum/directions";
 import { MeshReflectorMaterial } from "@react-three/drei";
@@ -37,13 +37,35 @@ export function RoomWall({ direction }: { direction: Direction }) {
     }
   }, [direction]);
 
+  const mirrorPosition: [x: number, y: number, z: number] = useMemo(() => {
+    switch (direction) {
+      case Direction.South:
+      case Direction.West:
+        return [0.1, 0.1, 0];
+      case Direction.North:
+      case Direction.East:
+        return [-0.1, 0.1, 0];
+    }
+  }, [direction]);
+
+  const mirrorRotation: [x: number, y: number, z: number] = useMemo(() => {
+    switch (direction) {
+      case Direction.South:
+      case Direction.West:
+        return [0, Math.PI / 2, 0];
+      case Direction.North:
+      case Direction.East:
+        return [0, -Math.PI / 2, 0];
+    }
+  }, [direction]);
+
   return (
     <group position={position} rotation={rotation}>
       <RigidBody type="fixed" colliders="cuboid">
         <mesh geometry={wallGeometry} material={wallMaterial} />
       </RigidBody>
 
-      <mesh position={[0.1, 0.1, 0.1]} geometry={mirrorGeometry} rotation={[0, Math.PI / 2, 0]}>
+      <mesh position={mirrorPosition} geometry={mirrorGeometry} rotation={mirrorRotation}>
         <MeshReflectorMaterial mirror={0.95} resolution={1024} />
       </mesh>
     </group>
